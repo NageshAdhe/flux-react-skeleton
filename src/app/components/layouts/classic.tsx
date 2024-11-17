effcc41b888788313effd803d08cfd7ead9931e7
nageshAdhe <nageshadhe@hotmail.com>
2024-11-17 16:50:51 +0530

    Working: styles update

--- src/app/components/layouts/classic.tsx
+++ src/app/components/layouts/classic.tsx
@@ -1,143 +1,161 @@
+import Link from "next/link";
 import Navigation from "../navigation/navigation";
-import "./classic.scss"
-const ClassicLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    const te = [
+import "./classic.scss";
+const ClassicLayout: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const te = [
+    {
+      id: "analytics",
+      title: "Analytics",
+      translate: "ANALYTICS",
+      type: "link",
+      icon: "link",
+      url: "/analytics",
+    },
+    {
+      id: "applications",
+      title: "Applications",
+      translate: "APPLICATIONS",
+      type: "group",
+      icon: "apps",
+      children: [
         {
-            id: 'analytics',
-            title: 'Analytics',
-            translate: 'ANALYTICS',
-            type: 'link',
-            icon: 'link',
-            url: '/analytics'
+          id: "academy",
+          title: "Academy",
+          translate: "ACADEMY",
+          type: "link",
+          icon: "whatshot",
+          url: "/academy",
         },
         {
-            id: 'applications',
-            title: 'Applications',
-            translate: 'APPLICATIONS',
-            type: 'group',
-            icon: 'apps',
-            children: [
-                {
-                    id: 'academy',
-                    title: 'Academy',
-                    translate: 'ACADEMY',
-                    type: 'link',
-                    icon: 'whatshot',
-                    url: '/academy'
-                },
-                {
-                    id: 'messenger',
-                    title: 'Messenger',
-                    translate: 'MESSENGER',
-                    type: 'link',
-                    icon: 'whatshot',
-                    url: '/messenger'
-                }
-            ]
+          id: "messenger",
+          title: "Messenger",
+          translate: "MESSENGER",
+          type: "link",
+          icon: "whatshot",
+          url: "/messenger",
+        },
+      ],
+    },
+    {
+      id: "ecommerce",
+      title: "E-Commerce",
+      translate: "E-COMMERCE",
+      type: "collapse",
+      icon: "apps",
+      children: [
+        {
+          id: "product",
+          title: "Product",
+          translate: "PRODUCT",
+          type: "link",
+          icon: "whatshot",
+          url: "/academy",
         },
         {
-            id: 'ecommerce',
-            title: 'E-Commerce',
-            translate: 'E-COMMERCE',
-            type: 'collapse',
-            icon: 'apps',
-            children: [
-                {
-                    id: 'product',
-                    title: 'Product',
-                    translate: 'PRODUCT',
-                    type: 'link',
-                    icon: 'whatshot',
-                    url: '/academy'
-                },
-                {
-                    id: 'productDetails',
-                    title: 'Product Details',
-                    translate: 'PRODUCT DETAILS',
-                    type: 'link',
-                    icon: 'whatshot',
-                    url: '/messenger'
-                }
-            ]
+          id: "productDetails",
+          title: "Product Details",
+          translate: "PRODUCT DETAILS",
+          type: "link",
+          icon: "whatshot",
+          url: "/messenger",
         },
+      ],
+    },
+  ];
+  return (
+    <div className=" flex h-screen overflow-hidden">
+      {/* LEFT SIDE BAR */}
+      <Navigation className="w-full" dense navigation={te} active={""} />
+      {/* RIGHT SIDE CONTENT */}
+      <div className="main-content">
+        <header className="header relative z-20 flex border-b sticky top-0">
+          <div className="toolbar container">
+            <div className="flex flex-row md:flex-row space-y-4 md:space-x-4 md:space-y-0  px-6">
+              <div className="w-full md:w-1/2  p-4 ">Div 1</div>
+              <div className="w-full md:w-1/2 p-4 ">Div 2</div>
+            </div>
+          </div>
+        </header>
 
-    ];
-    return (
-        <div className=" flex h-screen overflow-hidden">   
-         {/* LEFT SIDE BAR */}        
-            <Navigation className="w-full" dense navigation={te} active={""} />            
-            {/* RIGHT SIDE CONTENT */}
-            <div className="main-content">
-                <header className="header relative z-20 flex border-b sticky top-0">
-                    <div className="toolbar container">                        
-                        <div className="flex flex-row md:flex-row space-y-4 md:space-x-4 md:space-y-0">
-                        <div className="w-full md:w-1/2  p-4 ">Div 1</div>
-                        <div className="w-full md:w-1/2 p-4 ">Div 2</div>
-                        </div>
-                    </div>
-                </header>            
-           
-                <main className="content">
-                    <div className="Page-header flex container mx-auto mb-6">
-                        <div className="flex flex-row items-start space-y-8 sm:space-y-0 w-full sm:max-w-full min-w-0 px-6 pt-6">
-                          <div className="flex-col">
-                                <div >
-                                    <nav className="breadcrumb flex w-full mb-2" aria-label="breadcrumb">
-                                        <ol className="flex list-none m-0 p-0">
-                                            <li className="MuiBreadcrumbs-li">
-                                                <a className="block font-medium tracking-tight capitalize truncate" role="button" href="/">Home</a>
-                                            </li>
-                                            <li aria-hidden="true" className="px-2">/</li>
-                                            <li className="MuiBreadcrumbs-li">
-                                                <a className="block font-medium tracking-tight capitalize truncate" role="button" href="/apps">apps</a>
-                                            </li>
-                                            <li aria-hidden="true" className="px-2">/</li>
-                                            <li className="MuiBreadcrumbs-li">
-                                                <a className="block font-medium tracking-tight capitalize truncate" role="button" href="/apps/e-commerce">e-commerce</a>
-                                            </li>
-                                        </ol>
-                                        </nav>
-                                        </div>
-                            
-                                        <div className="flex items-center max-w-full space-x-12">
-                                            <div className="flex flex-col min-w-0 mb-6">
-                                                <p className="text-xl font-bold">A Walk Amongst Friends - Canvas Print</p>
-                                                <span className="MuiTypography-root MuiTypography-caption font-medium muiltr-1xaxt64">Product Detail</span>
-                                            </div>
-                                        </div>
-                          </div>
-                        </div>
-                        <div className="flex flex-1 w-full">
-                            BBBBBB
-                        </div>
-
-                    </div>
-                    <div className="container mb-6">
-                    {children}
-                    </div>
-                    
-                </main>
-           
-            <footer className="footer">
-                <div className="container">                        
-                    <div className="flex flex-row md:flex-row space-y-4 md:space-x-4 md:space-y-0">
-                        <div className="w-full md:w-1/2  p-4 ">Div 1</div>
-                        <div className="w-full md:w-1/2 p-4 ">Div 2</div>
-                    </div>
-                </div>
-            </footer> 
-            <div className="top-0 z-99">
-                <div id="fuse-settings-panel" className="setting-panel buttonWrapper">
-                    <button className="" type="button">
-                    <span className="material-icons">
-                    settings
+        <main className="content">
+          <div className="Page-header flex container mx-auto mb-6">
+            <div className="flex flex-row items-start space-y-8 sm:space-y-0 w-full sm:max-w-full min-w-0 px-6 pt-6">
+              <div className="flex-col">
+                <nav
+                  className="breadcrumb flex w-full mb-2"
+                  aria-label="breadcrumb"
+                >
+                  <ol className="flex list-none m-0 p-0">
+                    <li className="MuiBreadcrumbs-li">
+                      <Link
+                        className="block font-medium tracking-tight capitalize truncate"
+                        role="button"
+                        href="/"
+                      >
+                        Home
+                      </Link>
+                    </li>
+                    <li aria-hidden="true" className="px-2">
+                      /
+                    </li>
+                    <li className="MuiBreadcrumbs-li">
+                      <Link
+                        className="block font-medium tracking-tight capitalize truncate"
+                        role="button"
+                        href="/apps"
+                      >
+                        apps
+                      </Link>
+                    </li>
+                    <li aria-hidden="true" className="px-2">
+                      /
+                    </li>
+                    <li className="MuiBreadcrumbs-li">
+                      <Link
+                        className="block font-medium tracking-tight capitalize truncate"
+                        role="button"
+                        href="/apps/e-commerce"
+                      >
+                        e-commerce
+                      </Link>
+                    </li>
+                  </ol>
+                </nav>
+                <div className="flex items-center max-w-full space-x-12">
+                  <div className="flex flex-col min-w-0 mb-6">
+                    <p className="text-xl font-bold">
+                      A Walk Amongst Friends - Canvas Print
+                    </p>
+                    <span className="MuiTypography-root MuiTypography-caption font-medium muiltr-1xaxt64">
+                      Product Detail
                     </span>
-                    </button>                    
+                  </div>
                 </div>
-                </div>
-            </div>               
+              </div>
+            </div>
+            <div className="flex flex-1 w-full">BBBBBB</div>
+          </div>
+          <div className="container mb-6">{children}</div>
+        </main>
+        <footer className="footer">
+          <div className="container">
+            <div className="footer-content">
+              <a>test@asdasd.com</a>
+            </div>
+          </div>
+        </footer>
+        <div className="float-element">
+          <div id="fuse-settings-panel" className="setting-panel-btn-wrapper">
+            <button className="" type="button">
+              <span className="material-icons">settings</span>
+            </button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ClassicLayout;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,82 +1,76 @@
 // app/layout.tsx
 "use client";
-import StyledJsxRegistry from './registry'
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import theme from './core/Theme/fluxTheme';
+import StyledJsxRegistry from "./registry";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "./core/Theme/fluxTheme";
 
-import { useState } from 'react';
-import EmptyLayout from './components/layouts/empty';
-import DenseLayout from './components/layouts/dense';
-import ClassicLayout from './components/layouts/classic';
-import EnterPriseLayout from './components/layouts/enterprise';
-// import '../app/assets/styles/layout.scss';
-
+import { useState } from "react";
+import EmptyLayout from "./components/layouts/empty";
+import DenseLayout from "./components/layouts/dense";
+import ClassicLayout from "./components/layouts/classic";
+import EnterPriseLayout from "./components/layouts/enterprise";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [layout, setLayout] = useState<'empty' | 'dense' | 'classic' | 'enterprise'>('empty');
+  const [layout, setLayout] = useState<
+    "empty" | "dense" | "classic" | "enterprise"
+  >("empty");
 
-  const switchLayout = (layout: 'empty' | 'dense' | 'classic' | 'enterprise') => {
+  const switchLayout = (
+    layout: "empty" | "dense" | "classic" | "enterprise"
+  ) => {
     setLayout(layout);
   };
-let LayoutComponent = EmptyLayout;
-  switch(layout){
-    case 'dense':{
+  let LayoutComponent = EmptyLayout;
+  switch (layout) {
+    case "dense": {
       LayoutComponent = DenseLayout;
       break;
     }
-    case 'classic':{
+    case "classic": {
       LayoutComponent = ClassicLayout;
       break;
     }
-    case 'enterprise':{
+    case "enterprise": {
       LayoutComponent = EnterPriseLayout;
       break;
     }
-    default: { 
+    default: {
       LayoutComponent = ClassicLayout;
       break;
-   } 
+    }
   }
   // const LayoutComponent = layout === 'empty' ? EmptyLayout : DenseLayout;
 
   return (
     <html lang="en">
       <head>
-      <link
-  rel="stylesheet"
-  href="https://fonts.googleapis.com/icon?family=Material+Icons"
-/>
-<link
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+        <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Roboto:wght@400;700&display=swap"
           rel="stylesheet"
         />
-
       </head>
       <StyledJsxRegistry>
-      <body className='light'>
-              {/* <button onClick={() => switchLayout('empty')}>empty Layout</button>
-              <button onClick={() => switchLayout('dense')}>dense Layout</button>
-              <button onClick={() => switchLayout('classic')}>clssic Layout</button>
-              <button onClick={() => switchLayout('enterprise')}>enterprise Layout</button>      */}
-              <AppRouterCacheProvider options={{ key: 'css' }}>
-                <ThemeProvider theme={theme}>
-                  {/* <div className='splash-screen'></div> */}
-                  <div className='app-root'>  
-                    {/* <div className="layout">                   */}
-                    <LayoutComponent>{children}</LayoutComponent>
-                    {/* </div>             */}
-                  </div>            
-                </ThemeProvider>
-                </AppRouterCacheProvider>
-      </body>
+        <body className="light" data-theme="rose">
+        <div className="button">Test Button</div>
+          <AppRouterCacheProvider options={{ key: "css" }}>
+            <ThemeProvider theme={theme}>
+              <div className="app-root">
+                <LayoutComponent>{children}</LayoutComponent>
+              </div>
+            </ThemeProvider>
+          </AppRouterCacheProvider>
+        </body>
       </StyledJsxRegistry>
-     
     </html>
   );
 }
